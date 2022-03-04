ad33bcdaa4edd169a258f16392d4f9a73e7dae69
Idena-Egyptian-pharaoh <75703728+idena-Egyptian-Pharoh@users.noreply.github.com>
2022-03-04 21:10:55 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -61,13 +61,14 @@ function getInvitations(addy) {
 		while(i<30){
 			if(response.data["result"][i].type=="InviteTx"){
 				var invite=response.data["result"][i].to.toLowerCase();
+				console.log(getInvitations(addy))
 				//create buttons
 				allButtons=`${allButtons}<a href="https://scan.idena.io/address/${invite}" target="_blank" title="Open Idena explorer"><img src="https://robohash.idena.io/${invite}" width="50" height="50" class="img-circle border rounded-circle" style="background-color:white;"> <code>${invite}</code></a> <a class="btn btn-info btn-sm mt-1 md-1" onclick="deleteInvitation(\'${invite}\')" title="Delete invitation"><i class="far fa-trash-alt"></i></a><br><br>`;
 			}
 			i++;
 		}
 		document.getElementById('buttons').innerHTML=allButtons;
-		console.log(invite)
+		
 	});
 	
 }
